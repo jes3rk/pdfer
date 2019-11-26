e389fb8c229b9ac3e9c9e6b748e1758624887a0c
jes3rk <joseph.schefer@gmail.com>
2019-11-26 12:37:56 -0500

    Beginning working with Java Swing UI

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,15 @@
 package main;
 
-public final class Main {
+import javax.swing.JFrame;
 
+public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		JFrame f = new JFrame();
+		f.setSize(400, 500);
+		f.setLayout(null);
+		f.setVisible(true);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 }
